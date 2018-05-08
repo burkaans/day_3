dc497ebe5567d7621a7d5913bde61ef998fc06e6
burkaans <raimonds.buholts@gmail.com>
2018-05-08 19:26:58 +0300

    2. commit
    3. diena

--- day_3/day_3/Program.cs
+++ day_3/day_3/Program.cs
@@ -10,6 +10,36 @@ namespace day_3
     {
         static void Main(string[] args)
         {
+            Funkcija();
+
+            double rezultats = Funkcija2();
+            Console.WriteLine(rezultats);
+        
+
+        
+            Console.ReadLine();
+
         }
+
+        static void Funkcija()
+        {
+            int a, b, c;
+            a = 5;
+            b = 6;
+            c = a + b;
+            Console.WriteLine(c);
+          
+        }
+        static double Funkcija2()
+        {
+            double a, b, c;
+            a = 6.5;
+            b = 4.7;
+            c = a + b;
+
+            return c;
+              
+        }
+
     }
 }
